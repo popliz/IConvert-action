87681b08bb605bb3a88903dfbbd6280095c84a96
lizkes <erfan2333@gmail.com>
2022-08-21 16:09:38 +0800

    Update convert.py

--- app/libs/convert.py
+++ app/libs/convert.py
@@ -116,7 +116,7 @@ def ffmpeg_convert(video_path: Path):
           "-lag-in-frames",
           "25",
           "-crf",
-          "25",
+          "28",
           "-b:v",
           "0",
           "-g",
